87b28ff75887dfc3fbaf70bab8cc97a528f3b7ad
muhammedajmal953 <muhammedajmal953@gmail.com>
2024-04-21 12:01:55 +0530

    offer showing bug fixing

--- controller/userController/userProfileController.js
+++ controller/userController/userProfileController.js
@@ -124,8 +124,8 @@ exports.walletRecharge = async (req, res) => {
         "payment_method": "paypal"
       },
       "redirect_urls": {
-        "return_url": `http://www.thethirdeye.com/user//updateWallet?amount=${amount}`, // Your success URL
-        "cancel_url": 'http://www.thethirdeye.com/user/wallet'    // Your cancel URL
+        "return_url": `http://www.thethirdeye.shop/user//updateWallet?amount=${amount}`, // Your success URL
+        "cancel_url": 'http://www.thethirdeye.shop/user/wallet'    // Your cancel URL
       },
       "transactions": [{
         "amount": {
